c4b11ab835cf439241b7566764b774ebc614c38f
Tiger <tigerliu06@gmail.com>
2025-05-28 12:50:48 -0400

    added 46 py

---
+++ 46_Permutations.py
@@ -0,0 +1,15 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def backtracking(nums: List[int], level: int, permutations: List[List[int]]):
+            if level == len(nums) - 1:
+                permutations.append(nums[:]) 
+                return
+            for i in range(level, len(nums)):
+                nums[i], nums[level] = nums[level], nums[i] 
+                backtracking(nums, level + 1, permutations) 
+                nums[i], nums[level] = nums[level], nums[i] 
+
+    
+        permutations = []
+        backtracking(nums, 0, permutations)
+        return permutations
