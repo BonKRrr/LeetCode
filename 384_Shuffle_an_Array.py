0f91d8556e5629ba24a81f972d6d4bd9112da668
Tiger <tigerliu06@gmail.com>
2025-07-03 10:18:28 -0400

    added 384 py

---
+++ 384_Shuffle_an_Array.py
@@ -0,0 +1,23 @@
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.base = nums[:]
+
+    def reset(self) -> List[int]:
+        return self.base[:]
+
+    def shuffle(self) -> List[int]:
+        shuffled = self.base[:]
+        n = len(self.base)
+        for i in range(n - 1, -1, -1):
+            j = random.randint(0, i)
+            shuffled[i], shuffled[j] = shuffled[j], shuffled[i]
+        return shuffled
+        
+
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
