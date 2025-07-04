a60c6b48645925bdd8d45af30c6a1ed0aa138ea1
Tiger <tigerliu06@gmail.com>
2025-07-04 11:00:17 -0400

    added 528 py

---
+++ 528_Random_Pick_With_Weight.py
@@ -0,0 +1,17 @@
+class Solution:
+
+    def __init__(self, w: List[int]):
+        self.cumsum = w[:]
+        for i in range(1, len(w)):
+            self.cumsum[i] += self.cumsum[i - 1]
+
+
+    def pickIndex(self) -> int:
+        val = random.randint(1, self.cumsum[-1])
+        return bisect.bisect_left(self.cumsum, val, 0, len(self.cumsum))
+
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
