d75982b51451a2349841c9ca113eb914690fa4b1
Tiger <tigerliu06@gmail.com>
2025-07-16 11:14:15 -0400

    added 769 py

---
+++ 769_Max_Chunks_to_Make_Sorted.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxChunksToSorted(self, arr: List[int]) -> int:
+        
+        chunks, cur_max = 0, 0
+        for i, num in enumerate(arr):
+            cur_max = max(cur_max, num)
+            chunks += cur_max == i
+        return chunks
