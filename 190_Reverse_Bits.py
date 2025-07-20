5b50dd204ed13f4f983dccc19f6fb0807d6452cf
Tiger <tigerliu06@gmail.com>
2025-07-20 17:05:09 -0400

    optimize 190

--- 190_Reverse_Bits.py
+++ 190_Reverse_Bits.py
@@ -2,7 +2,9 @@ class Solution:
     def reverseBits(self, n: int) -> int:
         m = 0
         for _ in range(32):
+
             m = m << 1
             m += n & 1
             n = n >> 1
+            
         return m
