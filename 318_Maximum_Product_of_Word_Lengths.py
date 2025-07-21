8354144f8d8b64fb6efa6cae521b95fc892c1a16
Tiger <tigerliu06@gmail.com>
2025-07-21 10:57:17 -0400

    optimize 318

--- 318_Maximum_Product_of_Word_Lengths.py
+++ 318_Maximum_Product_of_Word_Lengths.py
@@ -4,7 +4,7 @@ class Solution:
         max_prod = 0
         for word in words:
             mask, w_len = 0, len(word)
-            
+
             for c in word:
                 mask = mask | (1 << (ord(c) - ord("a")))
             cache[mask] = max(cache.get(mask, 0), w_len)
@@ -12,5 +12,6 @@ class Solution:
             for h_mask, h_len in cache.items():
                 if (mask & h_mask) == 0:
                     max_prod = max(max_prod, w_len * h_len)
+                    
         return max_prod
         
