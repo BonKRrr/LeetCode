aea452ff62db5b2e7c9fed20f9ad7763cdd8c66a
Tiger <tigerliu06@gmail.com>
2025-07-22 10:31:12 -0400

    optimize 77

--- 77_Combinations.py
+++ 77_Combinations.py
@@ -10,7 +10,6 @@ class Solution:
                 backtracking(combinations, pick, i + 1, n, k) 
                 pick.pop()
 
-
         combinations = []
         pick = []
         backtracking(combinations, pick, 1, n, k)
