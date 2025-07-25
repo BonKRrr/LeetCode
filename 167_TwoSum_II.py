85a51cec07ad80edcb3212e4619df5d4ccfb5458
Tiger <tigerliu06@gmail.com>
2025-07-25 10:18:51 -0400

    optimize 167

--- 167_TwoSum_II.py
+++ 167_TwoSum_II.py
@@ -2,6 +2,7 @@ class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         i1 = 0
         i2 = len(numbers) - 1
+        
         while i1 < i2:
             if numbers[i1] + numbers[i2] == target:
                 ans = [i1 + 1, i2 + 1]
@@ -10,4 +11,5 @@ class Solution:
                 i1 += 1
             elif numbers[i1] + numbers[i2] > target:
                 i2 -= 1
+
         return ans
