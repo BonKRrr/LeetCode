6129c309662494b6ae2b2115bf3621a79bd634ba
Tiger <tigerliu06@gmail.com>
2025-07-27 10:20:04 -0400

    optimize 121

--- 121_Best_Time_to_Buy_and_Sell_Stock.py
+++ 121_Best_Time_to_Buy_and_Sell_Stock.py
@@ -1,7 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        
         buy, sell = -sys.maxsize, 0
         for price in prices:
             buy = max(buy, -price)
             sell = max(sell, buy + price)
+
         return sell
