eea47962bb27cf847ba556c30ee934d8d86a457b
Tiger <tigerliu06@gmail.com>
2025-07-28 10:19:56 -0400

    optimize 240

--- 240_Search_a_2D_Matrix.py
+++ 240_Search_a_2D_Matrix.py
@@ -1,5 +1,6 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
         m, n = len(matrix), len(matrix[0])
         i, j = 0, n - 1
         while i < m and j >= 0:
@@ -9,4 +10,5 @@ class Solution:
                 i += 1
             else:
                 j -= 1
+
         return False
