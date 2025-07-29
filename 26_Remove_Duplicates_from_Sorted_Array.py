2d9e5f2207aa2dc37923bcfc0aec898fe90b6597
Tiger <tigerliu06@gmail.com>
2025-07-29 11:09:09 -0400

    optimize 26

--- 26_Remove_Duplicates_from_Sorted_Array.py
+++ 26_Remove_Duplicates_from_Sorted_Array.py
@@ -1,6 +1,7 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         i = 1
+        
         while i<len(nums): #traverse array
             if nums[i] == nums[i-1]: #if the current element is same as previous, then remove
                 nums.pop(i)
