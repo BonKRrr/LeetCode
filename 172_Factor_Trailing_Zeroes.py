6fb6c8e3aafd9db5e062fe803e5ed337fdc196ae
Tiger <tigerliu06@gmail.com>
2025-07-31 10:13:39 -0400

    optimize 172

--- 172_Factor_Trailing_Zeroes.py
+++ 172_Factor_Trailing_Zeroes.py
@@ -1,5 +1,7 @@
 class Solution:
     def trailingZeroes(self, n: int) -> int:
+
         def trailingZeroes1(n):
             return 0 if n == 0 else n // 5 + trailingZeroes1(n // 5)
+        
         return trailingZeroes1(n)
