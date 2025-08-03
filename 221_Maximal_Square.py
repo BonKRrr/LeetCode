4539a1d2e041a4e57676069ddc7d9bf727ef1b12
Tiger <tigerliu06@gmail.com>
2025-08-03 10:32:33 -0400

    optimize 221

--- 221_Maximal_Square.py
+++ 221_Maximal_Square.py
@@ -7,4 +7,5 @@ class Solution:
             for j in range(1, n + 1):
                 if matrix[i - 1][j - 1] == "1":
                     dp[i][j] = min(dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j]) + 1
+                    
         return max(max(row) for row in dp) ** 2
