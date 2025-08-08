88bf50327e7fe565e40b378cad81b874d1ae156b
Tiger <tigerliu06@gmail.com>
2025-08-08 10:31:20 -0400

    optimize 232

--- 232_Implement_Queue_using_Stacks.py
+++ 232_Implement_Queue_using_Stacks.py
@@ -26,9 +26,3 @@ class MyQueue:
 
 
 
-# Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.peek()
-# param_4 = obj.empty()
