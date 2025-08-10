ffee1f16679a99009be4b82064f539120cd82db7
Tiger <tigerliu06@gmail.com>
2025-08-10 10:18:40 -0400

    optimize 135

--- 135_Candy.py
+++ 135_Candy.py
@@ -1,5 +1,6 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
+
         n = len(ratings)
         ans = [1] * n
         for i in range(1, n):
@@ -9,4 +10,5 @@ class Solution:
         for i in range(n - 1, 0, -1):
             if ratings[i] < ratings[i - 1]:
                 ans[i - 1] = max(ans[i - 1], ans[i] + 1)
+                
         return sum(ans)
