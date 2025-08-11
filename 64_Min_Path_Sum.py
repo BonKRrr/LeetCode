f154215a03e5f9f9d6574ab60d5d3a7ce5cb05ec
Tiger <tigerliu06@gmail.com>
2025-08-11 10:17:22 -0400

    optimize 64

--- 64_Min_Path_Sum.py
+++ 64_Min_Path_Sum.py
@@ -7,11 +7,15 @@ class Solution:
             for j in range(n):
                 if i == j == 0:
                     dp[i][j] = grid[i][j]
+                    
                 elif i == 0:
                     dp[i][j] = grid[i][j] + dp[i][j - 1]
+
                 elif j == 0:
                     dp[i][j] = grid[i][j] + dp[i - 1][j]
+
                 else:
                     dp[i][j] = grid[i][j] + min(dp[i][j - 1], dp[i - 1][j])
+                    
         return dp[m - 1][n - 1]
 
