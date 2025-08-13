6f160d6e252bf042539659f966b3ca93a21be0ec
Tiger <tigerliu06@gmail.com>
2025-08-13 10:11:56 -0400

    optimize 204

--- 204_Count_Primes.py
+++ 204_Count_Primes.py
@@ -2,8 +2,10 @@ class Solution:
     def countPrimes(self, n: int) -> int:
         if n <= 2:
             return 0
+        
         primes = [True for _ in range(n)]
         count = n - 2
+
         for i in range(2, n):
             if primes[i]:
                 for j in range(2 * i, n, i):
