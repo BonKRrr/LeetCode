cbe915e404f77f34686801b7822d98dec04942d1
Tiger <tigerliu06@gmail.com>
2025-08-16 10:20:04 -0400

    optimize 455

--- 455_assign_Cookies.py
+++ 455_assign_Cookies.py
@@ -1,5 +1,6 @@
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
+
         s.sort()
         g.sort()
         si = 0
@@ -7,8 +8,10 @@ class Solution:
         sl = len(s)
         gl = len(g)
         count = 0
+
         while (si < sl and gi < gl):
             if s[si] >= g[gi]:
                 gi += 1
             si += 1
+            
         return gi
