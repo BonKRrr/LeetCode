50e4ccfbd0cd9ab1f6276b337e7186883351db03
Tiger <tigerliu06@gmail.com>
2025-09-11 09:41:17 -0400

    optimize 1091

--- 1091_Shortest_Path_in_binary_Matrix.py
+++ 1091_Shortest_Path_in_binary_Matrix.py
@@ -8,21 +8,29 @@ class Solution:
         q.append((0, 0))
         grid[0][0] = -1 
         count = len(q)
+
         while count > 0:
             dist += 1
+
             while count > 0:
                 count -= 1
                 r, c = q.popleft()
+
                 if r == m - 1 and c == n - 1:
                     return dist
+                
                 for dx in range(-1, 2):
                     for dy in range(-1, 2):
                         if dx == 0 and dy == 0:
                             continue
+
                         x, y = r + dx, c + dy
+
                         if x < 0 or y < 0 or x >= m or y >= n or grid[x][y] != 0:
                             continue
+
                         grid[x][y] = -1
                         q.append((x, y))
+                        
             count = len(q)
         return -1
